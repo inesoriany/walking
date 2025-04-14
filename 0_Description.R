b57eb186038b3280ddc421a26380c99c5bbd4ad0
inesoriany <ines.khoun@ens.psl.eu>
2025-04-14 10:21:10 +0200

    walkers done

--- 0_Description.R
+++ 0_Description.R
@@ -41,24 +41,11 @@ emp_20_89 <-  emp %>%
 pop_tot <- sum(emp_20_89$pond_indc)
 pop_tot
 
-
 ################################################################################################################################
-#                                                          WALKING                                                             #
+#                                                   SURVEY DESIGNS                                                             #
 ################################################################################################################################
 
 
-# Creation of walking distances categories (in case but for now no need)
-emp_walkers <- emp_walkers %>% 
-  mutate(dist_grp = case_when(
-    nbkm_walking < 1                           ~  "0-1 km",
-    nbkm_walking >= 1 & nbkm_walking < 2       ~  "1-2 km",
-    nbkm_walking >= 2 & nbkm_walking < 5       ~  "2-5 km",
-    nbkm_walking >= 5 & nbkm_walking < 10      ~  "5-10 km",
-    nbkm_walking >= 10                         ~  "10 km +" 
-  ))%>% 
-  mutate(dist_grp = as.factor(dist_grp))
-
-    
 # Survey design ponderated by day
 ponderation_jour <- emp_walkers %>% 
   filter(pond_jour != "NA")
@@ -80,6 +67,39 @@ indiv <- pond_ind %>%
                    nest = TRUE)
 
 
+
+################################################################################################################################
+#                                                           HEALTH                                                             #
+################################################################################################################################
+
+## Mortality rates distribution per sex and age group
+mortality <- indiv %>% 
+  group_by(sexe, age_grp.x) %>% 
+  summarise(mort_rate_mean = survey_mean(mort_rate, proportion = TRUE, na.rm = TRUE))
+
+mean_mortality_rates <- ggplot(mortality, aes(x = age_grp.x, y = mort_rate_mean, fill = sexe)) +
+  geom_bar(width = 0.7, position = position_dodge2(0.7), stat = "identity") +
+  scale_fill_manual(values = c("Female" = "darkorange1",
+                               "Male" = "chartreuse4")) +
+  labs (y = "Mean mortality rate",
+        x ="Age group") +
+  theme_minimal()
+
+# Export plot
+ggsave(here("output", "Plot", "plot_mean_mortality_rates.tiff"), plot = mean_mortality_rates)
+
+
+
+
+################################################################################################################################
+#                                                          WALKING                                                             #
+################################################################################################################################
+
+
+##############################################################
+#                  TOTAL WALKED DISTANCE                     #
+##############################################################
+
 ## Total walked distance per day in 2019
 km_total_day <- svytotal(~nbkm_walking, jour)                   # Total km per day
 km_total_day
@@ -91,6 +111,34 @@ km_total_2019_IC <- confint(km_total_2019)                      # Confidence int
 svyby(~nbkm_walking, by = ~age_grp.x, jour, svytotal, na.rm = T)  
 
 
+
+# Creation of walking distances categories (in case but for now no need)
+emp_walkers <- emp_walkers %>% 
+  mutate(dist_grp = case_when(
+    nbkm_walking < 1                           ~  "0-1 km",
+    nbkm_walking >= 1 & nbkm_walking < 2       ~  "1-2 km",
+    nbkm_walking >= 2 & nbkm_walking < 5       ~  "2-5 km",
+    nbkm_walking >= 5 & nbkm_walking < 10      ~  "5-10 km",
+    nbkm_walking >= 10                         ~  "10 km +" 
+  ))%>% 
+  mutate(dist_grp = as.factor(dist_grp))
+
+## Plot : Proportion of people by distance walked
+proportion_km <- indiv %>% 
+  group_by(dist_grp = factor(dist_grp, levels = c("0-1 km", "1-2 km", "2-5 km", "5-10 km", "10 km +"))) %>%   # Put distance range in the right order 
+  summarise(total_pondere = survey_total (1, na.rm = TRUE)) %>%         # Sum of ponderation of individual for each distance group
+  mutate(proportion = total_pondere / sum(total_pondere))               # Proportion
+
+ggplot(proportion_km, aes(x = dist_grp, y = proportion)) +
+  geom_col() +
+  labs(title = "Proportion of people by distance walked",
+       x = "Distance range",
+       y = "Proportion")
+
+
+##############################################################
+#                    MEAN WALKED DISTANCE                    #
+##############################################################
 ## Mean km walked per day
 km_mean <- svymean(~nbkm_walking, jour, na.rm = TRUE)         # Mean km per day
 km_mean
@@ -132,43 +180,11 @@ ggsave(here("output", "Plot", "plot_mean_km_walkers.tiff"), plot = mean_km_walke
 
 
 
-## Plot : Proportion of people by distance walked
-proportion_km <- indiv %>% 
-  group_by(dist_grp = factor(dist_grp, levels = c("0-1 km", "1-2 km", "2-5 km", "5-10 km", "10 km +"))) %>%   # Put distance range in the right order 
-  summarise(total_pondere = survey_total (1, na.rm = TRUE)) %>%         # Sum of ponderation of individual for each distance group
-  mutate(proportion = total_pondere / sum(total_pondere))               # Proportion
-
-ggplot(proportion_km, aes(x = dist_grp, y = proportion)) +
-  geom_col() +
-  labs(title = "Proportion of people by distance walked",
-       x = "Distance range",
-       y = "Proportion")
- 
-
-
-
-## Mortality rates distribution per sex and age group
-mortality <- indiv %>% 
-  group_by(sexe, age_grp.x) %>% 
-  summarise(mort_rate_mean = survey_mean(mort_rate, proportion = TRUE, na.rm = TRUE))
-
-mean_mortality_rates <- ggplot(mortality, aes(x = age_grp.x, y = mort_rate_mean, fill = sexe)) +
-  geom_bar(width = 0.7, position = position_dodge2(0.7), stat = "identity") +
-  scale_fill_manual(values = c("Female" = "darkorange1",
-                               "Male" = "chartreuse4")) +
-  labs (y = "Mean mortality rate",
-        x ="Age group") +
-  theme_minimal()
-
-  # Export plot
-ggsave(here("output", "Plot", "plot_mean_mortality_rates.tiff"), plot = mean_mortality_rates)
-
-
-
 ################################################################################################################################
 #                                                          DRIVING                                                             #
 ################################################################################################################################
 
+# Total distance driven on all drivers
 
 # Proportion of the French adult population reporting any short (<5km) car trip in the past day
 # mean distance driven (km) in the past day among those reporting any car trip according to sex and age
