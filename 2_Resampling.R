4ead0e8266b4eb8dcd29a5f23af2726a9542b72d
inesoriany <ines.khoun@ens.psl.eu>
2025-05-14 18:23:32 +0200

    all cases + economic unit value

--- 2_Resampling.R
+++ 2_Resampling.R
@@ -7,15 +7,21 @@
   # EMP_walkers.xlsx
   # 0_Functions.R
 
-  # HIA_replicate.rds : results of HIA outputs for 100 replications of all diseases
-  # HIA_replicate_age.rds : results of HIA outputs for 100 replications per age of all diseases 
+  # HIA_replicate.rds : results of HIA outputs for 1000 replications of all diseases
+  # HIA_replicate_age.rds : results of HIA outputs for 1000 replications per age of all diseases 
 
 
 # Files outputted :
   # HIA_per_disease.xlsx : Prevented cases, DALY and costs per disease (IC from Monte-Carlo distribution)
   # HIA_per_disease_Rubin.xlsx : Prevented cases, DALY and costs per disease (IC from Rubin's rule)
+  # HIA_global.xlsx : Prevented cases, DALY and costs for all morbi-mortality events (IC from Monte-Carlo distribution)
+  # HIA_global_Rubin.xlsx : Prevented cases, DALY and costs for all morbi-mortality events (IC from Rubin's rule)
   # reduc_mortality_risk.xlsx : Reduction of mortality risk due to walking in 2019 (IC from Monte-Carlo distribution)
-  
+  # 1km_value_1000replicate.xlsx : Economic value of 1 km walked (medical costs) (in €)
+  # 1km_soc_value_1000replicate.xlsx : Economic value of 1 km walked (intangible costs) (in €)
+  # 1€_km_duration_1000replicate.xlsx : Distance and duration walked to save 1€ of medical costs
+  # soc_1€_km_duration_1000replicate.xlsx : Distance and duration walked to save 1€ of intangible costs
+
 
 ################################################################################################################################
 #                                                    1. LOAD PACKAGES                                                          #
@@ -137,7 +143,7 @@ calc_IC_Rubin(mortality, "tot_cases")                              # Rubin's rul
 set.seed(123)
 calc_replicate_IC(mortality, "tot_daly")                        # YLL prevented (distribution of Monte-Carlo replications)
 calc_IC_Rubin(mortality, "tot_daly")                               # Rubin's rule
-              
+
 
 
   # Morbidity 
@@ -150,8 +156,10 @@ calc_replicate_IC(morbidity, "tot_daly")                        # YLD prevented 
 calc_IC_Rubin(morbidity, "tot_daly")                               # Rubin's rule
 
 set.seed(123)
-calc_replicate_IC(morbidity, "tot_medic_costs")                 # Medical costs prevented (distribution of Monte-Carlo replications)
-calc_IC_Rubin(morbidity, "tot_medic_costs")                        # Rubin's rule
+calc_replicate_IC(morbidity, "tot_medic_costs")* 1e-6           # Medical costs prevented (in million €)
+calc_IC_Rubin(morbidity, "tot_medic_costs")* 1e-6                  # Rubin's rule
+
+
 
 
 
@@ -177,7 +185,14 @@ outcome_vec <- c("tot_cases", "tot_daly", "tot_medic_costs", "tot_soc_costs")
 
 # IC from distribution of Monte-Carlo replications
 set.seed(123)
-HIA_per_disease <- HIA_burden_IC(burden_disease, dis_vec, outcome_vec, calc_replicate_IC)
+HIA_disease <- HIA_burden_IC(burden_disease, dis_vec, outcome_vec, calc_replicate_IC)
+
+  # OTHER VISUALIZATION
+HIA_per_disease <- data.frame(disease = HIA_disease[["disease"]])
+for (out in outcome_vec){
+  HIA_per_disease <- HIA_per_disease %>% 
+    mutate(!!sym(out) := paste0(HIA_disease[[paste0(out)]], " (", HIA_disease[[paste0(out, "_low")]], " - ", HIA_disease[[paste0(out, "_sup")]], " )"))
+}
 
   # Export results 
 export(HIA_per_disease, here("output", "Tables", "Linear", "HIA_per_disease.xlsx"))
@@ -186,10 +201,69 @@ export(HIA_per_disease, here("output", "Tables", "Linear", "HIA_per_disease.xlsx
 
 
 # IC - Rubin's rule
-HIA_per_disease_Rubin <- HIA_burden_IC(burden_disease, dis_vec, outcome_vec, calc_IC_Rubin)
+HIA_disease_Rubin <- HIA_burden_IC(burden_disease, dis_vec, outcome_vec, calc_IC_Rubin)
+
+  # OTHER VISUALIZATION
+HIA_per_disease_Rubin <- data.frame(disease = HIA_disease_Rubin[["disease"]])
+for (out in outcome_vec){
+  HIA_per_disease_Rubin <- HIA_per_disease_Rubin %>% 
+    mutate(!!sym(out) := paste0(HIA_disease_Rubin[[paste0(out)]], " (", HIA_disease_Rubin[[paste0(out, "_low")]], " - ", HIA_disease_Rubin[[paste0(out, "_sup")]], " )"))
+}
 
   # Export results 
 export(HIA_per_disease_Rubin, here("output", "Tables", "Linear", "HIA_per_disease_Rubin.xlsx"))
+
+
+
+##############################################################
+#                         HIA GLOBAL                         #
+##############################################################
+
+# Global
+burden_global <- burden_replicate %>%
+  group_by(run) %>%
+  summarise(
+    tot_cases = sum(tot_cases, na.rm = TRUE),
+    tot_cases_se = sum(tot_cases_se, na.rm = TRUE),
+    tot_daly = sum(tot_daly, na.rm = TRUE),
+    tot_daly_se = sum(tot_daly_se, na.rm = TRUE),
+    tot_medic_costs = sum(tot_medic_costs * 1e-6, na.rm = TRUE),                        # in million € 
+    tot_medic_costs_se = sum(tot_medic_costs_se  * 1e-6, na.rm = TRUE),
+    tot_soc_costs = sum(tot_daly * vsl * 1e-6),
+    tot_soc_costs_se = sum(tot_daly_se * vsl * 1e-6)) %>% 
+  mutate(disease = "all")
+
+
+
+# IC from distribution of Monte-Carlo replications
+set.seed(123)
+HIA_global <- HIA_burden_IC(burden_global, "all", outcome_vec, calc_replicate_IC)
+
+  # OTHER VISUALIZATION
+HIA_all_disease <- data.frame(disease = HIA_global[["disease"]])
+for (out in outcome_vec){
+  HIA_all_disease_Rubin <- HIA_all_disease_Rubin %>% 
+    mutate(!!sym(out) := paste0(HIA_global[[paste0(out)]], " (", HIA_global[[paste0(out, "_low")]], " - ", HIA_global[[paste0(out, "_sup")]], " )"))
+}
+
+  # Export results 
+export(HIA_all_disease, here("output", "Tables", "Linear", "HIA_global.xlsx"))
+
+
+
+# IC - Rubin's rule
+HIA_global_Rubin <- HIA_burden_IC(burden_global, "all", outcome_vec, calc_IC_Rubin)
+
+  # OTHER VISUALIZATION
+HIA_all_disease_Rubin <- data.frame(disease = HIA_global_Rubin[["disease"]])
+for (out in outcome_vec){
+  HIA_all_disease_Rubin <- HIA_all_disease_Rubin %>% 
+    mutate(!!sym(out) := paste0(HIA_all_disease_Rubin[[paste0(out)]], " (", HIA_all_disease_Rubin[[paste0(out, "_low")]], " - ", HIA_all_disease_Rubin[[paste0(out, "_sup")]], " )"))
+}
+
+  # Export results 
+export(HIA_all_disease_Rubin, here("output", "Tables", "Linear", "HIA_global_Rubin.xlsx"))
+
 
 
 
@@ -231,11 +305,139 @@ export(reduc_mortality_risk_IC, here("output", "Tables", "Linear", "reduc_mortal
 
 
 
+################################################################################################################################
+#                                                 6. ECONOMIC UNIT VALUE (€)                                                   #
+################################################################################################################################
+
+# Survey design ponderated by day
+jour <- emp_walk %>% 
+  filter(pond_jour != "NA") %>% 
+  as_survey_design(ids = ident_ind,
+                   weights = pond_jour,
+                   strata = c(sexe, age_grp.x),
+                   nest = TRUE)
+
+# Total walked distance in 2019
+km_total_2019 <- as.numeric(svytotal(~nbkm_walking, jour)) *365.25/7                              # Total km per year
+km_total_2019_IC <- confint(svytotal(~nbkm_walking, jour) *365.25/7 )                             # Confidence interval
+
+
+# Setting parameters 
+km_low_2019 <- km_total_2019_IC[1, 1]
+km_sup_2019 <- km_total_2019_IC[1, 2]
+
+euro <- HIA_global[["tot_medic_costs"]] [[1]] * 1e6
+euro_low <- HIA_global[["tot_medic_costs_low"]] [[1]] * 1e6
+euro_sup <- HIA_global[["tot_medic_costs_sup"]] [[1]] * 1e6
+
+soc_euro <- HIA_global[["tot_soc_costs"]] [[1]] * 1e6
+soc_euro_low <- HIA_global[["tot_soc_costs_low"]] [[1]] * 1e6
+soc_euro_sup <- HIA_global[["tot_soc_costs_sup"]] [[1]] * 1e6
+
+
+
+##############################################################
+#                        VALUE OF 1km                        #
+##############################################################
+## MEDICAL COSTS ----
+
+# Calculate economic value of 1 km walked (medical costs) (in €)
+set.seed(123)
+unit_2019 <- unit_value(km_total_2019, km_low_2019, km_sup_2019, euro, euro_low, euro_sup, N=1000)
+unit_value_2019 <- as.data.frame(t(quantile(unit_2019, probs = c(0.025, 0.5, 0.975)))) %>% 
+  rename(euro_2.5 = "2.5%",
+         euro_50 = "50%",
+         euro_97.5 = "97.5%") %>% 
+  mutate(km = 1)
+
+
+# Export : economic value of 1 km walked per scenario
+export(unit_value_2019, here("output", "Tables", "Linear", "1km_value_1000replicate.xlsx"))
+
+
+
+## SOCIAL COSTS ----
+
+# Calculate economic value of 1 km walked (intangible costs) (in €)
+set.seed(123)
+unit_soc_2019 <- unit_value(km_total_2019, km_low_2019, km_sup_2019, soc_euro, soc_euro_low, soc_euro_sup, N=1000)
+unit_soc_value_2019 <- as.data.frame(t(quantile(unit_soc_2019, probs = c(0.025, 0.5, 0.975)))) %>% 
+  rename(soc_euro_2.5 = "2.5%",
+         soc_euro_50 = "50%",
+         soc_euro_97.5 = "97.5%") %>% 
+  mutate(km = 1)
+
+
+# Export : economic value of 1 km walked per scenario
+export(unit_soc_value_2019, here("output", "Tables", "Linear", "1km_soc_value_1000replicate.xlsx"))
+
+
+
+##############################################################
+#                           SAVE 1€                          #
+##############################################################
+
+
+## MEDICAL COSTS ----
+
+# Calculate distance walked to save 1€ of medical costs (km)
+set.seed(123)
+euro_2019 <- euro_unit(km_total_2019, km_low_2019, km_sup_2019, euro, euro_low, euro_sup, N = 1000)
+euro_unit_2019 <- as.data.frame(t(quantile(euro_2019, probs = c(0.025, 0.5, 0.975))))
+
+
+# Calculate duration walked to save 1€ of medical costs (min)
+euro_unit_duration_2019<- euro_unit_2019 %>% 
+  mutate(
+    min_2.5 = `2.5%` * 60 / walk_speed,
+    min_50 = `50%` * 60 / walk_speed,
+    min_97.5 = `97.5%` * 60 / walk_speed
+  ) %>% 
+  rename(km_2.5 = "2.5%",
+         km_50 = "50%",
+         km_97.5 = "97.5%") %>% 
+  mutate(medic_costs = 1)
+
+
+
+# Export : Calculate distance and duration to save 1€ of medical costs in 2019
+export(euro_unit_duration_2019, here("output", "Tables", "Linear", "1€_km_duration_1000replicate.xlsx"))
+
+
+
+
+## SOCIAL COSTS ----
+
+# Calculate distance walked to save 1€ of intangible costs (km)
+set.seed(123)
+soc_euro_2019 <- euro_unit(km_total_2019, km_low_2019, km_sup_2019, soc_euro, soc_euro_low, soc_euro_sup, N = 1000)
+soc_euro_unit_2019 <- as.data.frame(t(quantile(soc_euro_2019, probs = c(0.025, 0.5, 0.975))))
+
+
+# Calculate duration walked to save 1€ of intangible costs (min)
+soc_euro_unit_duration_2019<- soc_euro_unit_2019 %>% 
+  mutate(
+    min_2.5 = `2.5%` * 60 / walk_speed,
+    min_50 = `50%` * 60 / walk_speed,
+    min_97.5 = `97.5%` * 60 / walk_speed
+  ) %>% 
+  rename(km_2.5 = "2.5%",
+         km_50 = "50%",
+         km_97.5 = "97.5%") %>% 
+  mutate(soc_costs = 1)
+
+
+
+# Export : Calculate distance and duration to save 1€ of medical costs in 2019
+export(soc_euro_unit_duration_2019, here("output", "Tables", "Linear", "soc_1€_km_duration_1000replicate.xlsx"))
+
+
+
 
 
 
 ################################################################################################################################
-#                                                  6. DESCRIPTION (optional)                                                   #
+#                                                  7. DESCRIPTION (optional)                                                   #
 ################################################################################################################################
 
 # IMPORT DATA (to avoid re-generating Monte-Carlo replications)
