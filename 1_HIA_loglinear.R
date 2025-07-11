adaa9446c6b7671c40023cc134eddc5733d932b1
inesoriany <ines.khoun@ens.psl.eu>
2025-07-11 16:17:19 +0200

    new directories

--- 1_HIA_loglinear.R
+++ 1_HIA_loglinear.R
@@ -182,7 +182,7 @@ burden_IC <- burden_IC %>%
 
 
 # Export HIA : total of prevented cases, DALY and saved costs per disease
-export(burden_IC, here("output", "Tables", "Log linear", "Main analysis", "HIA_walking_2019.xlsx"))
+export(burden_IC, here("output", "Tables", "Log linear", "2019", "HIA_walking_2019.xlsx"))
 
 
 
@@ -236,7 +236,7 @@ burden_sex_IC <- burden_sex_age_IC %>%
             sup_medic_costs = sum(sup_medic_costs, na.rm = T))
 
   # Export HIA per sex : total of prevented cases, DALY and saved costs per disease
-export(burden_sex_IC, here("output", "Tables", "Log linear", "Main analysis", "HIA_sex_walking_2019.xlsx"))
+export(burden_sex_IC, here("output", "Tables", "Log linear", "2019", "HIA_sex_walking_2019.xlsx"))
 
 
 # Plot : Cases prevented by walking in 2019 according to sex 
@@ -251,7 +251,7 @@ cases_prevented <- ggplot(burden_sex_IC, aes(x = disease, y = tot_cases, ymin = 
 cases_prevented
 
   # Export plot
-ggsave(here("output", "Plots", "Log linear", "Main analysis", "plot_cases_prevented.png"), plot = cases_prevented)
+ggsave(here("output", "Plots", "Log linear", "2019", "plot_cases_prevented.png"), plot = cases_prevented)
 
 
 
@@ -286,7 +286,7 @@ daly_prevented <- ggplot(burden_age_IC, aes(x = age_grp.x, y = tot_daly, fill = 
 daly_prevented
 
   #Export plot
-ggsave(here("output", "Plots", "Log linear", "Main analysis", "plot_DALY_prevented.png"), plot = daly_prevented)
+ggsave(here("output", "Plots", "Log linear", "2019", "plot_DALY_prevented.png"), plot = daly_prevented)
 
 
 
@@ -345,7 +345,7 @@ cases_prevented_rev <- ggplot(burden_rev_IC, aes(x = disease, y = tot_cases, ymi
 cases_prevented_rev
 
 #Export plot
-ggsave(here("output", "Plots", "Log linear", "Main analysis", "plot_cases_per_rev.png"), plot = cases_prevented_rev)
+ggsave(here("output", "Plots", "Log linear", "2019", "plot_cases_per_rev.png"), plot = cases_prevented_rev)
 
 
 
@@ -404,7 +404,7 @@ deaths_prevented <- ggplot(prevented_death, aes(x = age_grp.x, y = tot_cases, ym
 deaths_prevented
 
 # Export plot
-ggsave(here("output", "Plots", "Log linear", "Main analysis", "plot_deaths_prevented.png"), plot = deaths_prevented)
+ggsave(here("output", "Plots", "Log linear", "2019", "plot_deaths_prevented.png"), plot = deaths_prevented)
 
 
 
@@ -426,7 +426,7 @@ YLL_prevented <- ggplot(prevented_death, aes(x = age_grp.x, y = tot_daly, ymin =
 YLL_prevented
 
 # Export plot : YLL prevented per sex by walking in 2019 
-ggsave(here("output", "Plots", "Log linear", "Main analysis", "plot_YLL_prevented.png"), plot = YLL_prevented)
+ggsave(here("output", "Plots", "Log linear", "2019", "plot_YLL_prevented.png"), plot = YLL_prevented)
 
 
 
@@ -477,7 +477,7 @@ unit_soc_value_2019 <- as.data.frame(t(quantile(unit_soc_2019, probs = c(0.025, 
 
     
 # Export : economic value of 1 km walked per scenario
-export(unit_soc_value_2019, here("output", "Tables", "Log linear", "Main analysis", "1km_soc_value.xlsx"))
+export(unit_soc_value_2019, here("output", "Tables", "Log linear", "2019", "1km_soc_value.xlsx"))
 
 
 
@@ -508,7 +508,7 @@ euro_unit_duration_2019<- euro_unit_2019 %>%
 
 
 # Export : Calculate distance and duration to save 1€ of medical costs in 2019
-export(euro_unit_duration_2019, here("output", "Tables", "Log linear", "Main analysis", "1€_km_duration.xlsx"))
+export(euro_unit_duration_2019, here("output", "Tables", "Log linear", "2019", "1€_km_duration.xlsx"))
 
 
 
@@ -536,7 +536,7 @@ soc_euro_unit_duration_2019<- soc_euro_unit_2019 %>%
 
 
 # Export : Calculate distance and duration to save 1€ of medical costs in 2019
-export(soc_euro_unit_duration_2019, here("output", "Tables", "Log linear", "Main analysis", "soc_1€_km_duration.xlsx"))
+export(soc_euro_unit_duration_2019, here("output", "Tables", "Log linear", "2019", "soc_1€_km_duration.xlsx"))
 
 
 
